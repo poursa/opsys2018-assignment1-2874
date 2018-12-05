e5bc8514e57719381085515e448db5f160fb0d04
poursa <porsa0@hotmail.com>
2018-12-05 03:19:00 +0200

    Finished script1a

--- script1a.sh
+++ script1a.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 # Leonidas Poursanidis 2874
 
+filename=$1
+LCOUNT=0
 
-#Getting the md5sum of a string
-getmd5(){
-	echo -n $1 | md5sum | sed 's/ -$//'
-
-}
-
-while read link; do #we do the checking for every link
+while read -r link; do #we do the checking for every link
+	LCOUNT=$((LCOUNT+1))
+	echo $LCOUNT
 	if [[ !($link =~ ^"#") ]]; then #Ignoring all comments in the read file
 		#looking to see if the link is already recorded
 		#     \/
-		if grep -qs "$link" hashes.txt; then #the link already existed before so we check the difference
-			echo "Found" #TODO: Check with previous
+		PREVLINKHASH=$(grep -s "$link" hashes.txt)
+		if [ $? -eq 0 ]; then #the link already existed before so we check the difference
+		
+			#We find the link's new hash or failure
+			REQUEST=$(curl -s $link 2>&1)
+			if [ $? -eq 0 ]; then #if the page is downloadable
+				#download the page, get its hash and assigne it to a variable
+				#the double inversion is there to wait for the curl to finish
+				TEMPHASHLINE="$link : $(echo $REQUEST | tac | tac | md5sum | sed 's/ -$//')"
+				#									/\
+				#    								||
+				#		Deleting the trailing - and space to get a bare md5sum
+			else #if the page fails then we store the failure
+				>&2 echo "$link FAILED" #output to stderr
+				TEMPHASHLINE="$link : FAILED"
+			fi
+			
+			##We compare the previous and current link hash/fail
+			if [ "$TEMPHASHLINE" = "$PREVLINKHASH" ]; then #THere is no change
+				echo "same"
+			else #There is a change so we update the velue of the hash of the site
+				echo "$link"
+				sed -i "${LCOUNT}s,.*,$TEMPHASHLINE," hashes.txt
+			fi
 		else #the link is new so we output its name along INIT
 			echo "$link INIT"
 			#We record the link's hash or failure
@@ -22,13 +42,13 @@ while read link; do #we do the checking for every link
 				#download the page, get its hash and throw it in a file
 				#the double inversion is there to wait for the curl to finish
 				echo "$link : $(echo $REQUEST | tac | tac | md5sum | sed 's/ -$//')" >> hashes.txt
-				#								/\
+				#									/\
 				#    								||
 				#		Deleting the trailing - and space to get a bare md5sum
 			else #if the page fails
-				echo "$link FAILED"
+				>&2 echo "$link FAILED" #output to stderr
 				echo "$link : FAILED" >> hashes.txt
 			fi
 		fi
 	fi
-done <links.txt
+done < "$filename"
